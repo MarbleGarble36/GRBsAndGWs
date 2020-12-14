c169d410483cc74d6cf483f9d9ed88387bc25cbf
Daan <daandietvorst@msn.com>
2020-12-14 16:34:06 +0100

    CHANGESSSS

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 
 # GRBsAndGWs
 
-This repository contains the latest versions of our models to predict Gamma-Ray Burst and Gravitational Wave event observation frequency. Filenames will change to make more sense later. For now, [test1.py](http://test1.py/) is our Gamma-Ray Burst model. Dependencies will be listed below.
+This repository contains the latest versions of our models to predict Gamma-Ray Burst and Gravitational Wave event observation frequency. Filenames will change to make more sense later. For now, [main.py](https://github.com/MarbleGarble36/GRBsAndGWs/blob/main/main.py) is our Gamma-Ray Burst model. Dependencies will be listed below.
 
-## Gamma-Ray Burst Model ([test1.py](http://test1.py/)):
+## Gamma-Ray Burst Model ([main.py](https://github.com/MarbleGarble36/GRBsAndGWs/blob/main/main.py)):
 
 A model that predicts the number of Gamma-Ray Bursts we can observe for a given inclination angle and sensor sensitivity. A list of formulas for Luminosity for given angle θ is in formulas.txt
 
--- main.py
+++ main.py
@@ -1,26 +1,21 @@
+from tkinter import *
 import tkinter as tk
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
 
 formulalist, graphlist = [], [] #List of used formulas for luminosity over angle and list of graph instances
 
-L0 = 1e52   #Starting luminosity
-t = np.arange(0, 1.6, .01)  #Theta variable
-Flim = 10 ** (-9)   #GRB Sensor sensitivity
-Glim = 10 ** (-22)   #GRB Sensor sensitivity
-
-f = open("formulas.txt", "r") #Read formulas from a text file (new ones are written to this file as well)
-for x in f:
+file = open("formulas.txt", "r") #Read formulas from a text file (new ones are written to this file as well)
+for x in file:
     formulalist.append(x)
-f.close()
+file.close()
 
 root= tk.Tk()   #Root window
 root.winfo_toplevel().title("GRB and GW Models")
+root.configure(bg='white')
 header = tk.Frame() #Header and label
-
 label = tk.Label(
     master=header,
     text="GRB and GW Models",
@@ -28,11 +23,10 @@ label = tk.Label(
     bg="white",
     width=10,
     height=2,
-    font=("Courier", 32)
+    font=("Helvetica", 32)
 )
 label.pack(fill=tk.X)
 header.pack(fill=tk.X)
-
 newformula = tk.Frame(bg="white")
 newformula.pack(fill=tk.X)
 
@@ -40,7 +34,7 @@ class ScrollableFrame(tk.Frame):    #Enables scrolling in the list of formulas
     def __init__(self, container, *args, **kwargs):
         super().__init__(container, *args, **kwargs)
         canvas = tk.Canvas(master=self, width=150, height=515)
-        scrollbar = tk.Scrollbar(self, orient="vertical", command=canvas.yview)
+        scrollbar = tk.Scrollbar(self, orient="vertical", command=canvas.yview, bg='white')
         self.scrollable_frame = tk.Frame(canvas)
         self.scrollable_frame.bind(
             "<Configure>",
@@ -49,43 +43,62 @@ class ScrollableFrame(tk.Frame):    #Enables scrolling in the list of formulas
             )
         )
         canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
-        canvas.configure(yscrollcommand=scrollbar.set)
+        canvas.configure(yscrollcommand=scrollbar.set, bg='white')
         canvas.pack(side="left", fill="both", expand=True)
         scrollbar.pack(side="left", fill="y")
-
 selection = ScrollableFrame(root)
 selection.pack(side=tk.LEFT, anchor="n")
 
-class GRBGraphObject:   #Graph object
+th = np.arange(0.01, np.pi/2, .01)  #Theta variable
+
+L0 = 1e52   #Starting luminosity
+Flim = 10 ** (-9)   #GRB Sensor sensitivity
+
+phi = np.pi/4 #Phase phi
+G = 6.67384e-11 #Gravitational Constant (from BiNaS)
+c = 299792458 #speed of light
+d = 10000   #Distance between the neutron stars in our binary system
+Glim = 10 ** (-22)   #GW Sensor sensitivity
+fs = 8677.86029     #Orbital frequency in our binary system
+Ms = 1.9884e30      #Mass of the sun
+M = 2.8 * Ms    #Mass of one neutron star
+SM = (M**2)**(3/5) * (2 * M)**(-1/5)    #Funky M
+
+class GRBGraphObject:   #Graph object for GRBs
     def __init__(self, formula):
         for i in graphlist:
             i.graph.pack_forget()
         self.graph = tk.Frame()
 
+        L = eval(formula)   #Luminosity over angle
+        dL = np.sqrt(L/(4*np.pi*Flim))   #Luminosity distance over angle
+        SA = 2*np.pi*(1-np.cos(th))   #Solid angle
+        nE = np.array(dL**3 * 2*np.pi*(1-np.cos(th))).cumsum()  #Number of events
+
         self.fig, self.ax = plt.subplots(2, 2)
-        self.fig.set_size_inches(5, 5)
+        self.fig.set_size_inches(7, 7)
         self.fig.subplots_adjust(bottom=0.12, hspace=0.42, right=0.96)
 
-        self.l1 = self.ax[0,0].plot(t, eval(formula))   #Luminosity over angle
-        self.l2 = self.ax[0,1].plot(t, np.sqrt(eval(formula)/(4*np.pi*Flim)))   #Luminosity distance over angle
-        self.l2 = self.ax[1,0].plot(t, 2*np.pi*(1-np.cos(t)))   #Solid angle
-        self.l2 = self.ax[1,1].plot(t, (np.sqrt(eval(formula)/(4*np.pi*Flim)))**3 * 2*np.pi*(1-np.cos(t)))  #Number of events
+        self.l1 = self.ax[0,0].plot(th, L)
+        self.l1 = self.ax[0,1].plot(th, dL)
+        self.l1 = self.ax[1,0].plot(th, SA)
+        self.l1 = self.ax[1,1].plot(th, nE)
 
-        self.ax[0,0].set(xlabel='Inclination angle (radians)', ylabel='Luminosity (erg/s)')
+        self.ax[0,0].set(xlabel='Inclination angle θ (radians)', ylabel='Luminosity (erg/s)')
         self.ax[0,0].set_title('Luminosity for given angle', y=1.04)
-        self.ax[0,1].set(xlabel='Inclination angle (radians)', ylabel='distance dL')
+        self.ax[0,1].set(xlabel='Inclination angle θ (radians)', ylabel='distance dL')
         self.ax[0,1].set_title('Luminosity distance for given angle', y=1.04)
-        self.ax[1,0].set(xlabel='t (radians)', ylabel='Solid Angle Ω')
-        self.ax[1,0].set_title('Solid Angle? for t', y=1.04)
-        self.ax[1,1].set(xlabel='t (radians)', ylabel='# events')
-        self.ax[1,1].set_title('Number of events?', y=1.04)
+        self.ax[1,0].set(xlabel='θ (radians)', ylabel='Solid Angle Ω')
+        self.ax[1,0].set_title('Solid Angle for θ', y=1.04)
+        self.ax[1,1].set(xlabel='θ (radians)', ylabel='# events')
+        self.ax[1,1].set_title('Number of events', y=1.04)
 
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.graph)
         self.canvas.draw()
         self.toolbar = NavigationToolbar2Tk(self.canvas, self.graph)
         self.toolbar.update()
 
-        self.button = tk.Button(selection.scrollable_frame, text=formula, width=20, command= lambda: self.change())
+        self.button = tk.Button(selection.scrollable_frame, text=formula, width=20, relief=GROOVE, command= lambda: self.change())
 
         self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
         self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
@@ -98,25 +111,34 @@ class GRBGraphObject:   #Graph object
         self.graph.pack(side=tk.RIGHT,anchor="n")
 
 class GWGraphObject:   #Graph object
-    def __init__(self, formula):
+    def __init__(self):
         self.graph = tk.Frame()
 
+        hp = (2*((G*M)**(5/3))*((np.pi*fs)**(2/3)))/(c**4 * d) * (1 + np.cos(th)**2) * np.cos(2 * phi) #Strain in hplus polarisation
+        hx = (-4*((G*SM)**(5/3))*((np.pi*fs)**(2/3)))/(c**4 * d) * np.cos(th) * np.sin(2*phi)   #Strain in hcross polarisation
+        h = np.sqrt(hp**2 + hx**2)  #Total Strain
+        dh = h/(4*np.pi*Glim)   #'Strain distance'
+        nE = ((1-np.cos(th))*dh**3).cumsum() #Relative number of events
+
         self.fig, self.ax = plt.subplots(2, 3)
-        self.fig.set_size_inches(8, 5)
+        self.fig.set_size_inches(10, 7)
         self.fig.subplots_adjust(bottom=0.12, hspace=0.42, left=0.074, right=0.96)
 
-        self.l1 = self.ax[0,0].plot(t, eval(formula))   #Luminosity over angle
-        self.l2 = self.ax[1,0].plot(t, eval(formula)/(4*np.pi*Glim))   #Luminosity distance over angle
-        self.l2 = self.ax[0,1].plot(t, 2*np.pi*(1-np.cos(t)))   #Solid angle
-        self.l2 = self.ax[0,2].plot(t, (np.sqrt(eval(formula)/(4*np.pi*Flim)))**3 * 2*np.pi*(1-np.cos(t)))  #Number of events
+        self.l1 = self.ax[0,0].plot(th, hp, color='red')
+        self.l1 = self.ax[0,1].plot(th, hx, color='red')
+        self.l1 = self.ax[0,2].plot(th, h, color='red')
+        self.l1 = self.ax[1,0].plot(th, dh, color='red')
+        self.l1 = self.ax[1,1].plot(th, nE, color='red')
 
-        self.ax[0,0].set(xlabel='Inclination angle (radians)', ylabel='Luminosity (erg/s)')
-        self.ax[0,0].set_title('Luminosity for given angle', y=1.04)
-        self.ax[0,1].set(xlabel='Inclination angle (radians)', ylabel='distance dL')
-        self.ax[0,1].set_title('Luminosity distance for given angle', y=1.04)
-        self.ax[1,0].set(xlabel='t (radians)', ylabel='Solid Angle Ω')
+        self.ax[0,0].set(xlabel='Angle i (radians)', ylabel='Strain')
+        self.ax[0,0].set_title('Strain in h+', y=1.04)
+        self.ax[0,1].set(xlabel='Angle i (radians)', ylabel='Strain')
+        self.ax[0,1].set_title('Strain in hx', y=1.04)
+        self.ax[0,2].set(xlabel='Angle i (radians)', ylabel='Strain')
+        self.ax[0,2].set_title('Total strain', y=1.04)
+        self.ax[1,0].set(xlabel='Angle i (radians)', ylabel='Distance dh (m)')
         self.ax[1,0].set_title('Distance', y=1.04)
-        self.ax[1,1].set(xlabel='t (radians)', ylabel='# events')
+        self.ax[1,1].set(xlabel='Angle i (radians)', ylabel='# events')
         self.ax[1,1].set_title('Number of events', y=1.04)
 
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.graph)
@@ -136,8 +158,8 @@ class CombiGraphObject:   #Graph object
         self.fig.set_size_inches(3, 5)
         self.fig.subplots_adjust(bottom=0.12, hspace=0.42)
 
-        self.l1 = self.ax[0].plot(t, eval(formula))   #Luminosity over angle
-        self.l2 = self.ax[1].plot(t, np.sqrt(eval(formula)/(4*np.pi*Flim)))   #Luminosity distance over angle
+        self.l1 = self.ax[0].plot(th, eval(formula))   #Luminosity over angle
+        self.l2 = self.ax[1].plot(th, np.sqrt(eval(formula)/(4*np.pi*Flim)))   #Luminosity distance over angle
 
         self.ax[0].set(xlabel='Inclination angle (radians)', ylabel='Luminosity (erg/s)')
         self.ax[0].set_title('Luminosity for given angle', y=1.04)
@@ -159,11 +181,11 @@ def new(newformula):
     f.write(newformula + "\n")
     f.close()
 
-CombiGraphs = CombiGraphObject("L0 * np.cos(t)")
-CombiGraphs.graph.pack(side=tk.RIGHT,anchor="n")
+#CombiGraphs = CombiGraphObject("L0 * np.cos(t)")
+#CombiGraphs.graph.pack(side=tk.RIGHT,anchor="n")
 
-Graphs = GWGraphObject("L0 * np.cos(t)")
-Graphs.graph.pack(side=tk.RIGHT,anchor="n")
+GWGraphs = GWGraphObject()
+GWGraphs.graph.pack(side=tk.RIGHT,anchor="n")
 
 for i in formulalist:
     graphlist.append(GRBGraphObject(i))
@@ -171,7 +193,7 @@ for i in formulalist:
 graphlist[0].graph.pack(side=tk.RIGHT,anchor="n")
 
 entry = tk.Entry(newformula,width=48)
-confirmbutton = tk.Button(newformula, text='Confirm', width=8, command= lambda: new(entry.get()))
+confirmbutton = tk.Button(newformula, text='Confirm', width=8, relief=GROOVE, command= lambda: new(entry.get()))
 entry.pack()
 confirmbutton.pack(anchor="e")
 
